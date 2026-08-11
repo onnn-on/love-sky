e3fd6fa4220f6bf0ab38c824eafae5b275e0ad99
苦瓜大王 <3771375891@qq.com>
2026-08-11 17:41:00 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const URL = "https://hfwfohoyuhmtmirrjye.csupabase.co";
+const URL = "https://hfwfohoyuhmtmirrjye.supabase.co";
 const KEY = "sb_publishable_nOodkqB5mk2KHSbBv9t19Q_7C-MmDjz";
 
 export const supabase = createClient(URL, KEY);
