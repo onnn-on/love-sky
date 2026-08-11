0fd8bf467eaef51ff97277e76dd90cd885b875df
苦瓜大王 <3771375891@qq.com>
2026-08-11 17:17:38 +0800

    Update Supabase URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const URL = "https://你的项目ID.supabase.co";
-const KEY = "你的anon key";
+const URL = "https://hfwfohoyuhmtmirrjye.csupabase.co";
+const KEY = "sb_publishable_nOodkqB5mk2KHSbBv9t19Q_7C-MmDjz";
 
 export const supabase = createClient(URL, KEY);
